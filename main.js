27f525d9a2fce2623ba4363929f65f0ee5bd2d35
ceej01 <ceej.carmean@tuta.io>
2018-01-09 21:35:49 -0600

    Added buttons and mouseover color change

--- main.js
+++ main.js
@@ -1,51 +1,65 @@
 //Evaluate what contianer will carry the grid
 container = document.getElementById('gridContainer');
-container.style.width = '600px';
+buildNewGrid = document.getElementById('reset');
+changeGridSize = document.getElementById('adjust');
+
+container.style.width = '780px';
+container.style.height = '780px';
+buildGrid(16);
+
+buildNewGrid.addEventListener("click", function() {
+	//getNewGridSize();
+	while (container.firstChild) {
+		container.removeChild(container.firstChild);
+	}
+	buildGrid(100);
+})
+
+changeGridSize.addEventListener("click", function() {
+	prompt("Enter New Size (0-100)");
+})
 
 function buildGrid(dimension) {
-	//create rows
-	for (var i = 0; i < dimension; i++) {
-		const rowContainer = document.createElement('div');
-		rowContainer.setAttribute('id', `col${i}`);
-		rowContainer.setAttribute('class', "column");
-		container.appendChild(rowContainer);
-		
-		//create pixels in rows
-		for (var j = 0; j < dimension; j++) {
-				const newPixel = document.createElement('div');
-				newPixel.setAttribute('id', `col${i}-row${j}`);
-				newPixel.setAttribute('class', "pixel");
-				
-				//modify each Pixel
-				modifyPixel(newPixel, dimension, 'red');
-				
-				rowContainer.appendChild(newPixel);
-			
 
-			}
+	const wrap = document.createElement('div');
+	wrap.setAttribute('class', "wrap");
+	//wrap.style.marginTop = '4px';
+	container.appendChild(wrap);
+
+	for (var i = 0; i < dimension ** 2; i++) {
+		const newPixel = document.createElement('div');
+		newPixel.setAttribute('id', `num${i}`);
+		newPixel.classList.add("pixel");
+		modifyPixel(newPixel, dimension, 'red');
+		wrap.appendChild(newPixel);
+
 	}
 
+colorPixels();
 }
+
+function colorPixels() {
+	let allSquares = document.querySelectorAll(".pixel");
+	allSquares.forEach((pixel) => {
+		pixel.addEventListener("mouseover", function() {
+			this.style.backgroundColor = 'black';
+		})
+	})
+}
+
 
 function modifyPixel(newPixel, dimension, color) {
 // Set, color, width, height
 	newPixel.style.backgroundColor = color;
-	
-	//get container size
-	var containerSize = parseInt(container.style.width, 10);
 
-	var styleString = (containerSize / dimension) - 2 + 'px';
+	var styleString = (parseInt(container.style.width, 10) / dimension) + 'px';
 	console.log(container.style.width);
-
 	newPixel.style.width = `${styleString}`;
 	newPixel.style.height = `${styleString}`;
-	//newPixel.style.heght = (400 / dimension);
-
+	//newPixel.style.marginTop = '-4px';
 }
-
-buildGrid(16);
 
 
 //add button to adjust grid dimensions
 //add button to change color
-// 
+
